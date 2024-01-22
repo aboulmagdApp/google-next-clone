fb63238e0d62a2a30e4b60227adfdf865109717b
aboulmagd <aboulmagd@live.com>
2024-01-22 09:49:37 +0300

    fetch data drom google search api

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({ searchParams }: any) {
+  const rsp = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await rsp.json();
+  const results = data.items;
+  return (
+    <>
+      {results && results.map((r: any) => <h1 key={r.cacheId}>{r.title}</h1>)}
+    </>
+  );
 }
