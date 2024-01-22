556ef4a4ba0e8363e46b546ae6e8561348c98f14
aboulmagd <aboulmagd@live.com>
2024-01-22 14:37:16 +0300

    update cloned by

--- src/components/CountryLookup.tsx
+++ src/components/CountryLookup.tsx
@@ -3,5 +3,10 @@ import React, { useState } from "react";
 
 export default function CountryLookup() {
   const [country, setCountry] = useState("Saudi Arabia");
-  return <div>{country}</div>;
+  return (
+    <div className="flex justify-between">
+      {country}
+      <p>cloned by: Mohamed Aboulmagd</p>
+    </div>
+  );
 }
