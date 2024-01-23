4fe2ea550c9655f4e21ff4f74e14b8d19f9fb3b1
aboulmagd <aboulmagd@live.com>
2024-01-23 16:40:21 +0300

    update error component

--- src/app/search/error.tsx
+++ src/app/search/error.tsx
@@ -2,13 +2,24 @@
 
 import { useEffect } from "react";
 
-export default function Error({ error, reset }: { error: Error; reset: any }) {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
   useEffect(() => {
     console.log("Error", error);
   }, [error]);
   return (
     <div className="flex flex-col justify-center items-center pt-10">
-      <h1 className="text-3xl mb-4">{error.message}</h1>
+      <h2 className="hidden text-3xl mb-4">{error.message}</h2>
+      <p className="text-red-500">
+        We apologize, but it seems that we have received Too Many Requests at
+        the moment. Please try again later. Thank you for your understanding and
+        patience.
+      </p>
       <button className="text-blue-500" onClick={() => reset()}>
         try again
       </button>
