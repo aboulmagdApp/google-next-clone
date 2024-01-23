93644bcd71d851366877f09359ef4df4180ec936
aboulmagd <aboulmagd@live.com>
2024-01-23 12:40:36 +0300

    update

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({ searchParams }: any) {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  //await new Promise((resolve) => setTimeout(resolve, 10000));
   const startIndex = searchParams.start || "1";
   const rsp = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
