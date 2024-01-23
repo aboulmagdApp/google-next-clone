00e8a17c7efd561c529ec71b9aaa9be4079d7858
aboulmagd <aboulmagd@live.com>
2024-01-23 15:16:56 +0300

    add aboulmagd logo

--- src/components/CountryLookup.tsx
+++ src/components/CountryLookup.tsx
@@ -1,15 +1,26 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+import Image from "next/image";
 
 export default function CountryLookup() {
   const [country, setCountry] = useState("Saudi Arabia");
   return (
     <div className="flex justify-between">
       {country}
-      <Link href="https://github.com/aboulmagdApp/google-next-clone">
-        cloned by: Mohamed Abouelmagd for learning purpose
-      </Link>
+      <div className="flex items-center">
+        <Link href="https://github.com/aboulmagdApp/google-next-clone">
+          <p className="text-blue-500 hover:underline">
+            cloned by: Mohamed Abouelmagd for learning purpose
+          </p>
+        </Link>
+        <Image
+          src="/images/aboulmagd.png"
+          alt="logo"
+          width={60}
+          height={60}
+        ></Image>
+      </div>
     </div>
   );
 }
